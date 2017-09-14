8b611ab7c5e3c0cb037d8a3d29c32a1bcd905a8c
Justin Lee <leejwg.3@gmail.com>
2017-09-13 19:09:31 -1000

    changed dot notation to [] notation

--- vault.js
+++ vault.js
@@ -4,19 +4,27 @@ module.exports = function() {
   var vault = {};
 
   var setValue = function(key, value) {
-    vault.key = value;
+    vault[key] = value;
    };
 
   var getValue = function(key){
-    if (vault.hasOwnProperty(key))
-        for (var e in vault){
-          var n = vault[e];
-          if (e === key){
-            return n;
-          }
-        } else{
-          return null;
-        }
-      };
-      getValue();
+    if (vault[key] === undefined){
+      return null;
+    }
+      // for (var e in vault){
+      //   var n = vault[e];
+      //   if (e === key){
+      //     return n;
+      //   }
+      // }
+     else{
+      return vault[key];
+    }
+
+  };
+     
+   return {
+    setValue: setValue,
+    getValue: getValue
+  };
 }; 
