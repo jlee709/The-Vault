3e52efbc11a8cb4bd3465ebcf554862af9faad78
Justin Lee <leejwg.3@gmail.com>
2017-09-13 18:27:45 -1000

    commited npm installs

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
+  
+  function getValue(key){}
+  
   var vault = {};
+
+  function setValue (key, value){
+    var myVault = Object[value];
+      return vault(key); {
+        
+        if (key === undefined){
+          return null;
+        }
+      }
+    }
+
+
 };
